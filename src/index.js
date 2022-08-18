f3d040d3b83b6001595899dc95bdf9b7a2499a56
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2022-08-18 22:06:47 +0530

    Authentication completed

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ require('dotenv').config();
 require('./database/connection')
 
 const express = require('express');
+const { requiresAuth } = require('express-openid-connect');
+
 const userRoutes = require('./routes/userDetail.route')
 const addressRoutes = require('./routes/address.route')
+const authentication = require('./auth/auth')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to KaraFi!')
-})
+app.use(authentication);
 
-app.use('/api', userRoutes);
+app.use('/api', requiresAuth(), userRoutes);
 
-app.use('/api', addressRoutes);
+app.use('/api', requiresAuth(), addressRoutes);
 
 app.use((req, res, next) => {
     res.status(404);
