9b12082ad5fa44f8e5c23db09f991b985a0762ab
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2022-09-06 10:37:52 +0530

    Init Roles completed

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,47 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
+const db = require('../authentication/models')
+
+const Role = db.role
+
 let url = process.env.DATABASE_URL
+
 mongoose.connect(url, {
 }).then(() => {
     console.log('Database Connection Successful!')
+    initial();
 }).catch((e) => {
     console.log(`Database connection failed: ${e}`)
+    process.exit()
 })
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                roleName: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'user' to roles collection");
+            });
+            new Role({
+                roleName: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'moderator' to roles collection");
+            });
+            new Role({
+                roleName: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
