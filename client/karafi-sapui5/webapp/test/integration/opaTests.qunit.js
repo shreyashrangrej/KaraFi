2157ed1c3292314486d8676b340be5e720b9396e
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2022-10-11 16:16:52 +0530

    Removed SAP UI5 cuz its shit

--- client/karafi-sapui5/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["com/karafi/karafisapui5/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- client/karafi-sapui5/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"comkarafi/karafi-sapui5/test/unit/controller/app.controller"
-], function () {
-	"use strict";
-});
