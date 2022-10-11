b8f009d5c5bbf1c99d71158b635aec7d68b55c03
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2022-10-11 16:36:31 +0530

    Started With nextjs

---
+++ client/karafi-nextjs/pages/login/index.js
@@ -0,0 +1,10 @@
+function LoginPage() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">KaraFi Login Page</h1>
+      </div>
+    )
+  }
+  
+  export default LoginPage;
+  
---
+++ client/karafi-nextjs/pages/signup/index.js
@@ -0,0 +1,9 @@
+function SignupPage() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">KaraFi Signup Page</h1>
+      </div>
+    )
+  }
+  
+  export default SignupPage;
