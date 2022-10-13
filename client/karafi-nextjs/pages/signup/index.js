34bff5b8c5662afe718edffa90bc37aed540cefe
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2022-10-13 10:59:34 +0530

    Removed Client completely coz addind new repo

--- client/karafi-nextjs/pages/signup/index.js
+++
@@ -1,9 +0,0 @@
-function SignupPage() {
-    return (
-      <div>
-        <h1 className="text-3xl font-bold underline">KaraFi Signup Page</h1>
-      </div>
-    )
-  }
-  
-  export default SignupPage;
