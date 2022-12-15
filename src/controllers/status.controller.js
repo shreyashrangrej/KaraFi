7e1ed181a76c333bdfa2dbc03082afd395e432cd
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2022-12-15 10:54:16 +0530

    Status Completed

--- src/controllers/status.controller.js
+++ src/controllers/status.controller.js
@@ -1,17 +1,83 @@
 const statusSchema = require('../models/status.model')
 const getStatuses = async (req, res, next) => {
-    return res.status(200).json({ message: 'get all status' })
+    try {
+        const status = await statusSchema.find()
+        res.status(200).send({ statuses: status })
+    } catch (error) {
+        return res.status(404).json({ Error: error.message })
+    }
 }
 const getStatusById = async (req, res, next) => {
-    return res.status(200).json({ message: 'get status by id' })
+    const id = req.params.id
+    let status
+    try {
+        status = await statusSchema.findOne({ statusId: id })
+    } catch (error) {
+        res.status(404).json({ Error: 'Something went wrong, could not find status: ' + id + ' .Please check logs.' })
+        return next()
+    }
+    if (!status) {
+        return res.status(404).json({ Error: 'Could not find the status for provided ID: ' + id })
+    }
+    res.status(200).json({ status: status })
 }
 const createStatus = async (req, res, next) => {
-    return res.status(200).json({ message: 'create status' })
+    const { statusId, statusName, statusDescription, projects, tasks, subTasks } = req.body
+    const createStatus = new statusSchema({
+        statusId,
+        statusName,
+        statusDescription,
+        projects,
+        tasks,
+        subTasks
+    })
+    try {
+        await createStatus.save()
+    } catch (error) {
+        res.status(500).json({ Error: error.message })
+        return next()
+    }
+    res.status(200).json({ status: createStatus })
 }
 const updateStatus = async (req, res, next) => {
-    return res.status(200).json({ message: 'update status' })
+    const { statusId, statusName, statusDescription, projects, tasks, subTasks } = req.body
+    const id = req.params.id
+    let status
+    try {
+        status = await statusSchema.findOne({ statusId: id })
+    } catch (error) {
+        res.status(500).json({ Error: 'Could not find the status for provided ID: ' + id + error.message })
+        return next()
+    }
+    status.statusId = statusId
+    status.statusName = statusName
+    status.statusDescription = statusDescription
+    status.projects = projects
+    status.tasks = tasks
+    status.subTasks = subTasks
+    try {
+        await status.save()
+    } catch (error) {
+        res.status(500).json({ Error: error.message })
+        return next()
+    }
+    res.status(200).json({ status: status })
 }
 const deleteStatus = async (req, res, next) => {
-    return res.status(200).json({ message: 'delete status' })
+    const id = req.params.id
+    let status
+    try {
+        status = await statusSchema.findOne({ statusId: id })
+    } catch (error) {
+        res.status(404).json({ Error: 'Cannot find status with provided ID: ' + id + error.message })
+        return next()
+    }
+    try {
+        await status.remove()
+    } catch (error) {
+        res.status(500).json({ Error: error.message })
+        return next()
+    }
+    res.status(200).json({ deletedstatus: status })
 }
 module.exports = { getStatuses, getStatusById, createStatus, updateStatus, deleteStatus }
