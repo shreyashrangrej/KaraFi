a9884a7aa0a7eef3f180320901435627c2377212
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2022-12-21 13:07:48 +0530

    started ejs for home page

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use('/', router)
 app.use((req, res, next) => {
     res.status(404);
     res.send({
-        error: "Not Found."
+        error: "Invaild Route or the Route does not exist."
     })
 })
 app.listen(port, () => {
