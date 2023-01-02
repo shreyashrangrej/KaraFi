c7418479a5e8e653e341cdc8685d5a02e50a778b
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2023-01-02 11:55:46 +0530

    removed check auth cuz no use

--- src/middleware/checkAuth.js
+++
@@ -1,7 +0,0 @@
-const checkAuth = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    return res.status(401).send({ message: 'Unauthorized' })
-}
-module.exports = { checkAuth }
