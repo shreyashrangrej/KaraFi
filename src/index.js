492c68b5f1130742ff61109f704b95428d22aa6a
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2023-01-02 14:33:14 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 const passport = require('./middleware/passport')
 const router = require('./routes/index')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`)
     next()
